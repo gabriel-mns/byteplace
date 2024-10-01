1833ca19cadf19f85a821fe4f87b698f09dcde32
Gabriel <gabrielgmns071204@gmail.com>
2024-10-01 19:15:54 -0300

    initial commit

---
+++ src/main/java/com/pucpr/byteplace/ByteplaceApplication.java
@@ -0,0 +1,13 @@
+package com.pucpr.byteplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucpr/byteplace/ByteplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucpr.byteplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
